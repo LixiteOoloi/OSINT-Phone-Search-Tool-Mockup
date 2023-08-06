061fe4cbe60542588211e8e5a3f66324ec2647a0
LixiteOoloi <140971910+LixiteOoloi@users.noreply.github.com>
2023-08-06 08:33:24 -0500

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,35 @@
+function dowebsite1(website1a, website1b, website1c)
+{
+    if(!confirm("This will open a new tab, click OK to continue")) {
+        return false;
+      }
+      {window.open('https://www.411.com/phone/1-' + website1a + '-' + website1b + '-' + website1c, 'Search01window');}
+    }
+  function dowebsite2(website2a, website2b, website2c)
+{
+    if(!confirm("This will open a new tab, click OK to continue")) {
+        return false;
+      }
+      {window.open('https://www.cyberbackgroundchecks.com/phone/' + website2a + '-' + website2b + '-' + website2c, 'Search02window');}
+    }
+  function dowebsite3(website3a, website3b, website3c)
+{
+    if(!confirm("This will open a new tab, click OK to continue")) {
+        return false;
+      }
+      {window.open('https://www.whitepages.com/phone/1-' + website3a + '-' + website3b + '-' + website3c, 'Search03window');}
+    }
+  function dowebsite4(website4a, website4b, website4c)
+{
+    if(!confirm("This will open a new tab, click OK to continue")) {
+        return false;
+      }
+      {window.open('https://numpi.com/phone-info/' + website4a + '' + website4b + '' + website4c, 'Search04window');}
+    }
+ function dowebsite5(website5a, website5b, website5c)
+{
+    if(!confirm("This will open a new tab, click OK to continue")) {
+        return false;
+      }
+      {window.open('https://thatsthem.com/phone/' + website5a + '-' + website5b + '-' + website5c, 'Search05window');}
+    }
